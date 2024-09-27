06679a9da0daa707e6b1946385f1f261a0ea46b4
Lamia Rizk <lamiarizk@gmx.de>
2024-09-27 17:29:15 +0200

    final

---
+++ Aufgaben_Tag25/Intro-TS-Level-1_12/src/basics/basics.ts
@@ -0,0 +1,27 @@
+// Aufgabe Intro-ts-level-1-12
+
+let dogName = "Leon";
+
+console.log(dogName);
+
+dogName = 11;
+
+console.log(dogName);
+
+dogName = "elf";
+
+console.log(dogName);
+
+// let age = "25";
+
+// console.log(age);
+
+// let age = 25;
+
+// console.log(age);
+
+// falsch, weil sagt NaN
+let number = 17;
+const sum = 17 + 44;
+console.log(sum);
+// console gibt 61, kann beide numbers addieren
