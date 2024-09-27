36cc2c5338dcf4fbc7e935617b85a1f0c442178f
Lamia Rizk <lamiarizk@gmx.de>
2024-09-27 18:00:00 +0200

    final

---
+++ Aufgaben_Tag25/Intro-TS-Level-1_13/src/basics/basics.ts
@@ -0,0 +1,24 @@
+// Addition
+let addition_operator: number = 1 + 1;
+console.log("addition: " + addition_operator);
+// Ergibt 2 in console
+
+// Subtraktion
+let subtraktion_operator: number = 2 - 1;
+console.log("subtraktion: " + subtraktion_operator);
+// Ergibt 1 in console
+
+// Multiplikation
+let multiplication_operator: number = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+// Ergibt 4 in console
+
+// Division
+let division_operator: number = 4 / 2;
+console.log("division: " + division_operator);
+// Ergibt 2 in console
+
+// Modulo: zeigt an, was der Rest sein würde.
+let modulo_operator: number = 14 % 5;
+console.log("modulus: " + modulo_operator);
+// Ergibt 4 in console
---
+++ Aufgaben_Tag25/Intro-TS-Level-1_13/src/main.ts
@@ -0,0 +1,13 @@
+import './style.css'
+// bis auf Zeile 1 alle import-Zeilen löschen
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="./src/basics/basics.html">meinLinkTypescript</a>
+  </div>
+`
+// oben das a tag einfügen zwischen die divs
+
+// alles zwischen diesen divs oben löschen
+// unten setup Zeile auch löschen
+
