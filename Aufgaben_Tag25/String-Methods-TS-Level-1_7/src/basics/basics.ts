70558431093ab7dd50f84735c9d9a66fd21afb3f
Lamia Rizk <lamiarizk@gmx.de>
2024-09-29 20:11:54 +0200

    bearbeitet

---
+++ Aufgaben_Tag25/String-Methods-TS-Level-1_7/src/basics/basics.ts
@@ -0,0 +1,11 @@
+const samsStatus: string = "Sam is good at codingschool";
+console.log(samsStatus.replace("good", "bad"));
+// output: Sam is bad at codingschool
+
+console.log(samsStatus.replace("Sam", "Susi"));
+// output: Susi is good at codingschool
+
+console.log(samsStatus.replace("codingschool", "tennis"));
+// output: Sam is good at tennis 
+
+
