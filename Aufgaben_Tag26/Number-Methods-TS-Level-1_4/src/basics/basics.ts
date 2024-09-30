27ba2dbd555f762747c8a134e6750b682ff5990e
Lamia Rizk <lamiarizk@gmx.de>
2024-09-30 15:22:33 +0200

    final

---
+++ Aufgaben_Tag26/Number-Methods-TS-Level-1_4/src/basics/basics.ts
@@ -0,0 +1,31 @@
+// Die Methode Number() konvertiert einen Wert zu einer Zahl.
+// Konvertiere anschließend die Werte mit der passenden Methode parseInt oder parseFloat
+
+const num1: number = 15.16698;
+console.log(Number(num1));
+
+const num2: number = 7.78714;
+console.log(Number(num2));
+
+const num3: string = "12.3";
+console.log(Number(num3));
+
+const num4: string = "3.14random";
+console.log(Number(num4));
+
+const num5: string = "32px";
+console.log(Number(num5));
+
+const num6: boolean = true;
+console.log(Number(num6));
+
+const num7: boolean = false;
+console.log(Number(num7));
+
+const num8: string = "321";
+console.log(Number(num8));
+
+const num: string = "Supercode";
+console.log(Number(num));
+
+//Da ich schon number benutze, brauche ich doch gar kein parseInt oder parseFloat?!
