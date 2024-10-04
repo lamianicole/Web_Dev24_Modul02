d6eaa365bf42850c0ed54289e80847a26096aeb5
Lamia Rizk <lamiarizk@gmx.de>
2024-10-04 12:53:14 +0200

    bearbeitet

---
+++ Aufgaben_Tag29/Array-TS-Level-1_1/src/basics/basics.ts
@@ -0,0 +1,22 @@
+const bucketItem1 = "London";
+const bucketItem2 = "Oslo";
+const bucketItem3 = "Montreal";
+
+const bucketList: string[] = ["London", "Oslo", "Montreal"];
+console.log(bucketList);
+
+
+const luckynum1 = 1;
+const luckynum2 = 2;
+const luckynum3 = 3;
+
+const luckyNumbers: number[] = [1, 2, 3];
+console.log(luckyNumbers);
+
+
+const person1 = "Sarah";
+const person2 = "Nicolas";
+const person3 = "John";
+
+const favoritePeople: string[] = ["Sarah", "Nicolas", "John"];
+console.log(favoritePeople);
