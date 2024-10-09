a216b50992326b4bb2e74297a19c757827fda864
Lamia Rizk <lamiarizk@gmx.de>
2024-10-09 08:57:13 +0200

    under constr

--- Aufgaben_Tag31/10xFun-with-Callback-functions/src/callback/callback.ts
+++ Aufgaben_Tag31/10xFun-with-Callback-functions/src/callback/callback.ts
@@ -1,89 +1,101 @@
-// //function vs. arrow function
-// const sum = function(a, b) {
-//     return a + b;
-// }
+//function vs. arrow function
+const sum = function(a, b) {
+    return a + b;
+}
 
-// // ... ist das Gleiche wie Arrow function (Kurzschreibweise):
-// const sum = (a, b) => {
-//     return a + b;
-// }
+// ... ist das Gleiche wie Arrow function (Kurzschreibweise):
+const sum = (a, b) => {
+    return a + b;
+}
 
-// //...oder noch kürzer:
-// const sum = (a, b) => a + b;
-
-
-
-// // forEach()
-// const fruits: string[] = ["Apfel", "Banane", "Kirsche"];
-
-// fruits.forEach((fruit) => {
-//     console.log(fruit);
-// });
-// //   ergibt:
-// //   Apfel
-// //   Banane
-// //   Kirsche
+//...oder noch kürzer:
+const sum = (a, b) => a + b;
 
 
-// // map()
-// const uppercasedFruits: string[] = fruits.map((fruit) => fruit.toUpperCase());
 
-// console.log(uppercasedFruits); 
-// // ["APFEL", "BANANE", "KIRSCHE"]
+// forEach()
+const fruits: string[] = ["Apfel", "Banane", "Kirsche"];
 
-
-// // filter()
-// const fruitsWithA: string[] = fruits.filter((fruit) => fruit.includes("a"));
-
-// console.log(fruitsWithA); // ["Banane"]
-
-// // const fruitsWithA: string[] = fruits.filter((fruit) => fruit.includes("A"));
-// // console.log(fruitsWithA); // ['Apfel'] 
+fruits.forEach((fruit) => {
+    console.log(fruit);
+});
+//   ergibt:
+//   Apfel
+//   Banane
+//   Kirsche
 
 
-// // forEach()
-// const numbers: number[] = [1, 2, 3, 4, 5];
+// map()
+const uppercasedFruits: string[] = fruits.map((fruit) => fruit.toUpperCase());
 
-// numbers.forEach((number) => {
-//     console.log(number);
-// });
-// // ergibt:
-// // 1
-// // 2
-// // 3
-// // 4
-// // 5
+console.log(uppercasedFruits); 
+// ["APFEL", "BANANE", "KIRSCHE"]
 
-// // map()
-// let doubledNumbers: number[] = numbers.map((number) => 
-//     number * 2);
 
-// console.log(doubledNumbers); // [2, 4, 6, 8, 10]
+// filter()
+const fruitsWithA: string[] = fruits.filter((fruit) => fruit.includes("a"));
+
+console.log(fruitsWithA); // ["Banane"]
+
+// const fruitsWithA: string[] = fruits.filter((fruit) => fruit.includes("A"));
+// console.log(fruitsWithA); // ['Apfel'] 
+
+
+// forEach()
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+numbers.forEach((number) => {
+    console.log(number);
+});
+// ergibt:
+// 1
+// 2
+// 3
+// 4
+// 5
+
+// map()
+let doubledNumbers: number[] = numbers.map((number) => 
+    number * 2);
+
+console.log(doubledNumbers); // [2, 4, 6, 8, 10]
 
 
 // sort() Compare function
-// const musicians: string[] = ["Doja Cat", "Aqua Stone Throne", "Nils Frahm", "Ruger Hauer", "The Wanton Bishops"];
-// console.log(musicians);
+const musicians: string[] = ["Doja Cat", "Aqua Stone Throne", "Nils Frahm", "Lagwagon", "Ruger Hauer", "The Wanton Bishops", "Vitalic", "Descendents"];
+console.log(musicians);
 
 
-// const musiciansUp = (a: string, b: string) => {
-//     return a.localeCompare(b);
-// }
+const musiciansUp = (a: string, b: string) => {
+    return a.localeCompare(b);
+}
 
-// console.log(musicians.sort(musiciansUp));
+console.log(musicians.sort(musiciansUp));
 
-// const musiciansDown = (a: string, b: string) => {
-//     return b.localeCompare(a);
-// }
+const musiciansDown = (a: string, b: string) => {
+    return b.localeCompare(a);
+}
 
-// console.log(musicians.sort(musiciansDown));
+console.log(musicians.sort(musiciansDown));
 
 
 // length (nach Zeichenanzahl sortieren)
-// const musicians: string[] = ["Doja Cat", "Aqua Stone Throne", "Nils Frahm", "Ruger Hauer", "The Wanton Bishops"];
-// console.log(musicians);
+const musicians: string[] = ["Doja Cat", "Aqua Stone Throne", "Nils Frahm", "Ruger Hauer", "The Wanton Bishops"];
+console.log(musicians);
 
-// console.log(musicians.sort((a: string, b: string) => a.length - b.length));
-// console.log(musicians.sort((a: string, b: string) => b.length - a.length));
-// console.log(musicians.sort());
+console.log(musicians.sort((a: string, b: string) => a.length - b.length));
+console.log(musicians.sort((a: string, b: string) => b.length - a.length));
+console.log(musicians.sort());
+
+
+const DosGamesChildhood: string[] = ["Duke Nukem", "Cosmo's Cosmic Adventure", "Commander Keen", "Doom", "Wolfenstein", "Megarace", "California Games" ];
+console.log(DosGamesChildhood.includes("Doom"));
+//true
+console.log(DosGamesChildhood.includes("Jill of the Jungle"));
+// false
+console.log(DosGamesChildhood.slice(2));
+//['Commander Keen', 'Doom', 'Wolfenstein', 'Megarace', 'California Games']
+console.log(DosGamesChildhood.slice(2, 3));
+// ['Commander Keen']
+
 
